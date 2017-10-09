7c48a1c6867a849f840edd32264977469be88793
ranjanan <benditlikeranjan@gmail.com>
2017-10-09 16:47:55 +0530

    Reduce memory in strength matrix calc

--- src/strength.jl
+++ src/strength.jl
@@ -7,9 +7,6 @@ Classical(;θ = 0.25) = Classical(θ)
 function strength_of_connection{T, Ti, Tv}(c::Classical{T}, A::SparseMatrixCSC{Tv, Ti})
 
     θ = c.θ
-    #I = Int[]
-    #J = Int[]
-    #V = Float64[]
 
     m, n = size(A)
     nz = nnz(A)
@@ -19,17 +16,15 @@ function strength_of_connection{T, Ti, Tv}(c::Classical{T}, A::SparseMatrixCSC{T
     k = 1
 
     for i = 1:n
-        neighbors = A[:,i]
-        _m = find_max_off_diag(neighbors, i)
+        #neighbors = A[:,i]
+        #_m = find_max_off_diag(neighbors, i)
+        _m = find_max_off_diag(A, i)
         threshold = θ * _m
         for j in nzrange(A, i)
             row = A.rowval[j]
             val = A.nzval[j]
             if abs(val) >= threshold
                 if row != i
-                    #=push!(I, row)
-                    push!(J, i)
-                    push!(V, abs(val))=#
                     I[k] = row
                     J[k] = i
                     V[k] = abs(val)
@@ -38,9 +33,6 @@ function strength_of_connection{T, Ti, Tv}(c::Classical{T}, A::SparseMatrixCSC{T
             end
 
             if row == i
-                #=push!(I, row)
-                push!(J, i)
-                push!(V, val)=#
                 I[k] = row
                 J[k] = i
                 V[k] = val
@@ -60,19 +52,41 @@ function strength_of_connection{T, Ti, Tv}(c::Classical{T}, A::SparseMatrixCSC{T
     S', S
 end
 
-function find_max_off_diag(neighbors, col)
+#=function find_max_off_diag(neighbors, col)
     maxval = zero(eltype(neighbors))
     for i in 1:length(neighbors.nzval)
         maxval = max(maxval, ifelse(neighbors.nzind[i] == col, 0, abs(neighbors.nzval[i])))
     end
     return maxval
+end=#
+function find_max_off_diag(A, i)
+    m = 0
+    for j in nzrange(A, i)
+        row = A.rowval[j]
+        val = A.nzval[j]
+        if row != i
+            m = max(m, abs(val))
+        end
+    end
+    m
+end
+
+function find_max(A, i)
+    m = zero(eltype(A))
+    for j in nzrange(A, i)
+        row = A.rowval[j]
+        val = A.nzval[j]
+        m = max(m, abs(val))
+    end
+    m
 end
 
 function scale_cols_by_largest_entry!(A::SparseMatrixCSC)
 
     n = size(A, 1)
     for i = 1:n
-        _m = maximum(A[:,i])
+        #_m = maximum(A[:,i])
+        _m = find_max(A, i)
         for j in nzrange(A, i)
             A.nzval[j] /= _m
         end
