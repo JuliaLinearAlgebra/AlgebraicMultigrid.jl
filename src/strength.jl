4b7fb4fbbcce0bbfcc320dd9f17a6b85f815e9a3
ranjanan <benditlikeranjan@gmail.com>
2017-10-09 16:51:11 +0530

    Cleanup

--- src/strength.jl
+++ src/strength.jl
@@ -16,8 +16,6 @@ function strength_of_connection{T, Ti, Tv}(c::Classical{T}, A::SparseMatrixCSC{T
     k = 1
 
     for i = 1:n
-        #neighbors = A[:,i]
-        #_m = find_max_off_diag(neighbors, i)
         _m = find_max_off_diag(A, i)
         threshold = θ * _m
         for j in nzrange(A, i)
@@ -48,17 +46,9 @@ function strength_of_connection{T, Ti, Tv}(c::Classical{T}, A::SparseMatrixCSC{T
 
     scale_cols_by_largest_entry!(S)
 
-
     S', S
 end
 
-#=function find_max_off_diag(neighbors, col)
-    maxval = zero(eltype(neighbors))
-    for i in 1:length(neighbors.nzval)
-        maxval = max(maxval, ifelse(neighbors.nzind[i] == col, 0, abs(neighbors.nzval[i])))
-    end
-    return maxval
-end=#
 function find_max_off_diag(A, i)
     m = 0
     for j in nzrange(A, i)
@@ -76,7 +66,7 @@ function find_max(A, i)
     for j in nzrange(A, i)
         row = A.rowval[j]
         val = A.nzval[j]
-        m = max(m, abs(val))
+        m = max(m, val)
     end
     m
 end
@@ -85,12 +75,10 @@ function scale_cols_by_largest_entry!(A::SparseMatrixCSC)
 
     n = size(A, 1)
     for i = 1:n
-        #_m = maximum(A[:,i])
         _m = find_max(A, i)
         for j in nzrange(A, i)
             A.nzval[j] /= _m
         end
     end
-
     A
 end
