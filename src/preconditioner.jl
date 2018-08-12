f0f22f0eb75cdc31cb586ac3ec301d2cda8346f6
mohamed82008 <mohamed82008@gmail.com>
2018-08-12 17:11:22 +0200

    Fix solve and preconditioner API allowing different initializations

--- src/preconditioner.jl
+++ src/preconditioner.jl
@@ -1,19 +1,42 @@
-struct Preconditioner
-    ml::MultiLevel
+struct Preconditioner{ML<:MultiLevel}
+    ml::ML
+    init::Symbol
 end
+Preconditioner(ml) = Preconditioner(ml, :zero)
 
 aspreconditioner(ml::MultiLevel) = Preconditioner(ml)
 
 @static if VERSION < v"0.7-"
     import Base: \, *, A_ldiv_B!, A_mul_B!
-    A_ldiv_B!(x, p::Preconditioner, b) = copyto!(x, p \ b)
+    function A_ldiv_B!(x, p::Preconditioner, b)
+        if p.init == :zero
+            x .= 0
+        else
+            x .= b
+        end
+        solve!(x, p.ml, b, maxiter = 1, calculate_residual = false)
+    end
     A_mul_B!(b, p::Preconditioner, x) = A_mul_B!(b, p.ml.levels[1].A, x)
 else
     import Compat.LinearAlgebra: \, *, ldiv!, mul!
     ldiv!(p::Preconditioner, b) = copyto!(b, p \ b)
-    ldiv!(x, p::Preconditioner, b) = copyto!(x, p \ b)
+    function ldiv!(x, p::Preconditioner, b)
+        if p.init == :zero
+            x .= 0
+        else
+            x .= b
+        end
+        solve!(x, p.ml, b, maxiter = 1, calculate_residual = false)
+    end
     mul!(b, p::Preconditioner, x) = mul!(b, p.ml.levels[1].A, x)
 end
 
-\(p::Preconditioner, b) = solve(p.ml, b, maxiter = 1, calculate_residual = false)
+function \(p::Preconditioner, b)
+    if p.init == :zero
+        x = zeros(b)
+    else
+        x = copy(b)
+    end
+    solve!(x, p.ml, b)
+end
 *(p::Preconditioner, b) = p.ml.levels[1].A * x
