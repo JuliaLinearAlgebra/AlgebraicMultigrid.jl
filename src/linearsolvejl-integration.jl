3c1fe8962646e0ba3d00822fb9af25abb21b805f
termi-official <termi-official@users.noreply.github.com>
2026-02-16 15:10:39 +0100

    Remove more debris

--- src/linearsolvejl-integration.jl
+++
@@ -1,7 +0,0 @@
-struct LinearSolveAlgebraicMultigrid_JL{ML} <: SciMLBase.AbstractLinearAlgorithm
-    multilevel::ML
-end
-
-function init_cacheval(alg::LinearSolveAlgebraicMultigrid_JL, A, b, u, Pl, Pr, maxiters, abstol, reltol, verbose)
-    lu!(convert(AbstractMatrix, A))
-end
